fa9ea4ec1d7111b5aa7e8a0e68a0447dad0ba391
ocsoares <casoares016@gmail.com>
2023-12-29 13:38:59 -0300

    feat: add getAll method to ProductController

--- src/main/java/com/ocsoares/aprendendospringboot/controllers/ProductController.java
+++ src/main/java/com/ocsoares/aprendendospringboot/controllers/ProductController.java
@@ -7,9 +7,12 @@ import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -29,5 +32,12 @@ public class ProductController {
         ProductModel savedProduct = productRepository.save(productModel);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
+    }
+
+    @GetMapping("product")
+    public ResponseEntity<List<ProductModel>> getAll() {
+        List<ProductModel> productModelList = productRepository.findAll();
+
+        return ResponseEntity.status(HttpStatus.OK).body(productModelList);
     }
 }
