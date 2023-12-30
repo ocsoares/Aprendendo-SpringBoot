9b341730d33ab1d51b74ae6e5baa7f34239b2fed
ocsoares <casoares016@gmail.com>
2023-12-29 21:20:35 -0300

    feat: add Data lobok annotation to ProductModel

--- src/main/java/com/ocsoares/aprendendospringboot/models/ProductModel.java
+++ src/main/java/com/ocsoares/aprendendospringboot/models/ProductModel.java
@@ -1,6 +1,7 @@
 package com.ocsoares.aprendendospringboot.models;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -8,6 +9,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "TB_PRODUCTS")
+@Data
 public class ProductModel implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L; // Número de Controle de VERSÃO da Classe!!
@@ -16,28 +18,4 @@ public class ProductModel implements Serializable {
     private UUID id;
     private String name;
     private Double price;
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
 }
